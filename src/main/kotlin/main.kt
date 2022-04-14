7c96349f54404d51b269ae0a34946f9bfa09de00
@2444evelyn <everlinetsuma@gmail.com>
2022-04-14 09:41:10 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,52 @@
+fun main(){
+   var Currentacc=currentAccount(97,"lily",300)
+
+
+}
+class currentAccount(var acountnumber:Int, var accountname:String, var balance:Int){
+   fun deposit(amount:Double){
+      balance+amount
+      println(deposit(99.00))
+   }
+   fun withdraw(amount:Double){
+      balance-amount
+      println(withdraw(100.00))
+
+   }
+   fun details(currentAccount:Int){
+      val x = accountname
+      val y = balance
+
+
+   }
+
+}
+class Savingaccount (var acountnumber:Int, var accountname:String, var balance:Int){
+   fun deposit(amount:Double){
+      println(deposit(99.00))
+   }
+   fun withdraw(amount:Double){
+      println(withdraw(100.00))
+
+
+   }
+   fun details(name:Int){
+      val x =accountname
+      val y = balance
+   }
+   fun withdrawals(money:Int){
+
+   }
+}
+data class product (var name:String,var weight:Int,var price:Int,var category:String)
+//fun takesproduct():product{
+
+
+
+fun Char(character:String):String{
+character.forEachIndexed
+
+
+
+}
+
